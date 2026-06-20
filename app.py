716ef7ab114d9ae247162bd6af65be0c06b22763
Aakanksha Chavan <aakankshachavan511@gmail.com>
2026-06-21 01:55:38 +0530

    to test if it throws error

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+        @app.route('/')
+    def hello_world():
+            return render_template('index.html')
+        return none
 
 
 @app.route('/health')
